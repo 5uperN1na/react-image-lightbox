8fcb93ce5e89f2833aef5bec86770c67a78c9cbf
Paula Suarez <pauladzuck@hotmail.com>
2021-01-26 08:24:09 -0600

    Adding updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
+
+const images = [
+  '//placekitten.com/1500/500',
+  '//placekitten.com/4000/3000',
+  '//placekitten.com/800/1200',
+  '//placekitten.com/1500/1500',
+];
 
 function App() {
+  const [photoIndex, setPhotoIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+
+  const handleLightbox = () => {
+    setIsOpen (!isOpen)
+
+  }
+ 
+  const handlePreview = () => {
+    setPhotoIndex ((photoIndex + images.length - 1) % images.length)
+  }
+
+  const handleNext = () => {
+    setPhotoIndex ((photoIndex + 1) % images.length)
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <button type="button" onClick={handleLightbox}>
+        Open Lightbox
+        </button>
+
+      {isOpen && (
+        <Lightbox
+          mainSrc={images[photoIndex]}
+          nextSrc={images[(photoIndex + 1) % images.length]}
+          prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+          
+          onCloseRequest={handleLightbox}
+
+        
+          onMovePrevRequest={handlePreview}
+         
+          onMoveNextRequest={handleNext}
+
+        />
+      )}
     </div>
   );
 }
